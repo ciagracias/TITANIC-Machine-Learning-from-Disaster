ac6aaa9832c31e915d23f5362711986d4319b923
Gracia Rizka Pasfica <124945882+ciagracias@users.noreply.github.com>
2023-02-18 16:35:22 +0700

    Update Classification.py

--- Classification.py
+++ Classification.py
@@ -5,9 +5,13 @@ import matplotlib.pyplot as plt
 import seaborn as sns 
 
 # Classification with GaussianNB
-classifier = GaussianNB
+classifier = GaussianNB()
 classifier.fit(x_train, y_train) # Train a classification models
 classifier.score(x_train, y_train) # Accuracy
+
+# Predicting the Test Set result
+y_pred = classifier.predict(x_train) 
+y_pred # Show the result
 
 # Accuracy with Confusion Matrix
 f, ax = plt.subplots(figsize=(5,5))
@@ -22,10 +26,6 @@ plt.xlabel("Predicted Label")
 plt.ylabel("True Label")
 plt.show()
 
-# Predicting the Test Set result
-y_pred = classifier.predict(x_test) 
-y_pred # Show the result
-
-# Predictin the Test Set result one by one
+# PredictinG the Test Set result one by one
 y_pred = classifier.predict([x_test[12]]) # indexing to 12
 y_pred # Show the result
